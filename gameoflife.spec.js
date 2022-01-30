27a25c29cdd399aeda83de2d32777f8e5299c55e
Mel Escolano <amalia.escolano@gmail.com>
2022-01-30 17:47:18 +0100

    Improve testing Jest to 96.66%

--- gameoflife.spec.js
+++ gameoflife.spec.js
@@ -1,4 +1,9 @@
-import { whoIsLive, searchCellsAliveArround, newBorn } from "./gameoflife.js";
+import {
+  whoIsLive,
+  gameOfLife,
+  searchCellsAliveArround,
+  newBorn,
+} from "./gameoflife.js";
 
 describe("testing searchCellsAliveArround", () => {
   test("If ejeX = 1, ejeY = 1, result should be 2", () => {
@@ -30,11 +35,23 @@ describe("testing searchCellsAliveArround", () => {
   });
 });
 
+describe("testing gameOfLife", () => {
+  test("If arrayLife is [[1, 2],[2, 2],[3, 2]], result should be undefined", () => {
+    const arrayLife = [
+      [1, 2],
+      [2, 2],
+      [3, 2],
+    ];
+    expect(gameOfLife(arrayLife)).toBe(undefined);
+  });
+});
+
 describe("testing newBorn", () => {
   test("If ejeX = 2, ejeY = 2, result should be [[1, 2], [3, 2]]", () => {
-    expect(newBorn(2, 2)).toEqual([
+    expect(newBorn(2, 2)).toStrictEqual([
       [1, 2],
-      [3, 2],
+      [3, 1],
+      [3, 3],
     ]);
   });
   test("If ejeX = -1, ejeY = -1, result should be []", () => {
